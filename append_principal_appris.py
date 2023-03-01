11c59f800b516d0ad9dc6adf2c07b5d41db012ba
codemeleon <anmol.kiran@gmail.com>
2023-03-01 11:21:02 +0000

    python3 print

--- append_principal_appris.py
+++ append_principal_appris.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlitedict import SqliteDict
 
 sqlite_db = SqliteDict(sys.argv[2])
-tranlist =  sqlite_db["transcripts"]
+tranlist = sqlite_db["transcripts"]
 
 transhelve = sqlite3.connect(sys.argv[1])
 cursor = transhelve.cursor()
@@ -12,10 +12,12 @@ transhelve.commit()
 
 cursor.execute("SELECT transcript,gene,length from transcripts")
 result = cursor.fetchall()
-print "tranlist done"
+print("tranlist done")
 
 #print "UPDATE transcripts SET principal = 1 WHERE transcript IN ({});".format(str(tranlist).strip("[]"))
-cursor.execute("UPDATE transcripts SET principal = 1 WHERE transcript IN ({});".format(str(tranlist).strip("[]")))
+cursor.execute(
+    "UPDATE transcripts SET principal = 1 WHERE transcript IN ({});".format(
+        str(tranlist).strip("[]")))
 
 transhelve.commit()
 transhelve.close()
