1e06d550291b333ed0e4dd25c3f37fe858c8fbd5
codemeleon <anmol.kiran@gmail.com>
2023-04-11 16:57:52 +0100

    hide/unhide samples/projects

--- static/js/aks/interaction.js
+++ static/js/aks/interaction.js
@@ -1,5 +1,26 @@
-function all_on() {}
+// Display or hide samples and project
+$('input[name="seq_type"]').on("click", function() {
+                let seq_type = $('input[name = "seq_type"]:checked').val();
+                $(document).ready(function() { // Toggles project and samples visibility
+                                $('input[id$="project"]').each(function(project) { // Project visibility
+                                                if (project.startsWith(seq_type)) {
+                                                                project.style.display = "block";
+                                                } else {
+                                                                project.style.display = "none";
+                                                }
+                                });
+                                $('input[id$="sample"]').each(function(sample) { // Sample visibility
+                                                if (sample.startsWith(seq_type)) {
+                                                                sample.style.display = "block";
+                                                } else {
+                                                                sample.style.display = "none";
+                                                }
+                                });
+                                //alert(content_type);
 
-function all_off() {
+                });
+});
 
-}
+$("").on("click", function() {
+                // Select or deselect sample	
+})
