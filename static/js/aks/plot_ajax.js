e27da73d8893950ea2d973d35bcc1d3d830968dd
codemeleon <anmol.kiran@gmail.com>
2024-06-16 15:41:51 +0100

    parsing form with lists

--- static/js/aks/plot_ajax.js
+++ static/js/aks/plot_ajax.js
@@ -8,6 +8,35 @@ $("#query").click(() => {
   //  console.log(JSON.stringify(formJson));
   $.post("/query", formJson, (data) => {
     parse(data, "plot");
-    //alert(data);
+    // alert(data);
+  });
+});
+
+$("#comparequery").click(() => {
+  const formJson = $("#form")
+    .serializeArray()
+    .reduce((json, { name, value }) => {
+      json[name] = value;
+      return json;
+    }, {});
+  $("#form")
+    .find("ul.file")
+    .each(function () {
+      const list_name = $(this).attr("name");
+      const lst = [];
+      $(this)
+        .find("li")
+        .each(function () {
+          lst.push($(this).attr("name"));
+        });
+      if (lst.length > 0) {
+        formJson[list_name] = lst;
+      }
+    });
+
+  console.log(JSON.stringify(formJson));
+  $.post("/comparequery", JSON.stringify(formJson), (data) => {
+    parse(data, "plot");
+    // alert(data);
   });
 });
