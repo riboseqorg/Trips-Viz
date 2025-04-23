dfc58131ff881f2cc298a5ae3be45aace104c80f
codemeleon <anmol.kiran@gmail.com>
2025-04-23 10:52:36 +0100

    cleaning and annotation helper integration

--- riboflask.py
+++ riboflask.py
@@ -1,16 +1,15 @@
 import collections
 import os
 
-import altair as alt
-import config
-import fixed_values
 import numpy as np
 import polars as pl
+from sqlitedict import SqliteDict
+
+import config
+import fixed_values
 from core_functions import sequence2rdg
 from fetch_shelve_reads2 import get_reads
 from fixed_values import get_user_defined_seqs
-from plots import VegaPlot
-from sqlitedict import SqliteDict
 from sqlqueries_2 import get_table, sqlquery
 
 
@@ -21,18 +20,6 @@ def generate_plot(data, settings) -> str:
         return ("Error: Cannot display Ribo-Seq Coverage when 'Line Graph'" +
                 " is turned off")
 
-    # Label and visibility for the interactive legends
-    labels_visibility = {
-        "Frame 1 profiles": True,
-        "Frame 2 profiles": True,
-        "Frame 3 profiles": True,
-        "RNA": True,
-        "Exon Junctions": True,
-        "CDS markers": True,
-    }
-    if "mismatches" in data:
-        for nuc in "ATGC":
-            labels_visibility[f"Mismatches {nuc}"] = False
     # This is a list of booleans that decide if the interactive legends boxes are filled in or not.Needs to be same length as labels
     if data['owner'] == 1:
         sqlpath = "{0}/{1}/{2}/{2}.{3}.sqlite".format(config.SCRIPT_LOC,
@@ -48,7 +35,6 @@ def generate_plot(data, settings) -> str:
             data["transcriptome"])
     traninfo = sqlquery(sqlpath, "transcripts").filter(
         pl.col("transcript") == data["transcript"])[0].to_dicts()[0]
-    print(traninfo, 'ha ha ha')
     data["tranlen"] = traninfo['length']
     for ss in ['start_list', 'stop_list', 'exon_junctions']:
         try:
@@ -58,7 +44,7 @@ def generate_plot(data, settings) -> str:
     # TODO: Replace next with fill na in dataframe
     if not traninfo["cds_start"]:
         traninfo["cds_start"] = 0
-    if not traninfo["cds_stop"]:
+    # if not traninfo["cds_stop"]:
         traninfo["cds_stop"] = 0
 
     try:
@@ -74,50 +60,20 @@ def generate_plot(data, settings) -> str:
     data["coding_regions"] = coding_regions
     data["traninfo"] = traninfo
 
-    seq = traninfo["sequence"].upper(
-    )  # NOTE: I guess it is already upper case
 
-    start_dataframe = pl.DataFrame({
-        'frame': ((traninfo['start_list'] - 1) % 3) + 1,
-        'type': ['start'] * len(traninfo['start_list']),
-        'pos':
-        traninfo['start_list']
-    })
-    # TODO: refine start stops
-    stop_dataframe = []
-    orf_dataframe = []
-    for i in range(len(seq)):
-        if seq[i:i + 3] in ["TAG", "TAA", "TGA"]:
-            frame = (i % 3) + 1
-            stop_dataframe.append([frame, seq[i:i + 3], i + 1])
-            orf_dataframe.append([
-                frame,
-                start_dataframe.filter((pl.col('frame') == frame)
-                                       & (pl.col('pos') < i + 1)).select(
-                                           pl.last('pos'))[0], i + 1
-            ])
-    stop_dataframe = pl.DataFrame(stop_dataframe,
-                                  schema=["frame", "type", "pos"])
-    orf_dataframe = pl.DataFrame(orf_dataframe,
-                                 schema=["frame", "start", "pos"])
-    # Error occurs if one of the frames is empty for any given start/stop, so we initialise with -5 as this won't be seen by user and will prevent the error
-    start_stop_dataframe = pl.DataFrame({
-        'frame': [1, 2, 3],
-        'type': ['start'] * 3,
-        'pos': [-5] * 3
-    })
-    start_stop_dataframe = pl.concat(
-        [start_stop_dataframe, start_dataframe, stop_dataframe])
-    start_stop = start_stop_dataframe.with_columns(
-        pl.col("type").map_elements(lambda x: x if x == 'start' else 'stop'))
 
+
+
+    # TODO: I need to orf in sqlite file
+    orfs = SqliteDict(sqlpath.replace(".sqlite","_helper.sqlite")) # TODO:: Check if it will work 
+    start_stop_dataframe = pl.DataFrame(orfs["start_stop_list"], schema=["frame", "pos", "type"])
+    orf_dataframe = pl.DataFrame(orfs["orfs"], schema=["frame", "start", "stop"])
     # TODO: add avarible of rnaseq
 
     all_rna_reads, rna_seqvar_dict = get_reads(
         data
     )  # TODO: keep rna_Seqvar_dict and ribo_seq_var_dict meltated to compine them
     all_rna_reads = all_rna_reads.with_columns(frame=pl.col("pos") % 3 + 1)
-    frame_3 = 0 if 'show_nuc' in data else 3
     
     rdg = str(sequence2rdg(seq))
     # print(rdg)
